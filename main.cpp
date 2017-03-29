d978b0948e4dcd7b4962087828040a0a07307475
tangzongzhi <tangzongzhi@gmail.com>
2017-03-29 14:23:42 -0400

    Rewrite cmakelist for root and subsudinary.

--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 //This is a HelloWorld test!
 #include<stdio.h>
 #include<iostream>
-#include"CPPBasicFunction.cpp"
+#include "src/CPPBasicFunction.cpp"
 using namespace std;
 // typedef struct Node{
 //     int number;
@@ -12,24 +12,7 @@ using namespace std;
 
 int main(){
     printf("Hello World!\n");
-    // usleep(100000);
 
-    // vector<string> test;
-    // test.push_back("a");
-    // string value=longestCommonPrefix(test);
-    // cout<<value<<endl;
-    // Node *pHead;
-    // Node *pHead2;
-    // *pHead=*pHead2;
-    // for(int i=0;i<20;i++){
-    //     pHead->number=i;
-    //     pHead=pHead->next;
-    // }
-    // while(pHead2->next!=NULL){
-    //     printf("%d",pHead2->number);
-    //     pHead2=pHead2->next;
-    // }
-
-    CPPBasicFunction();
+    run_CPPBasicFunction();
     return 0;
 }
--- src/CPPBasicFunction.cpp
+++ src/CPPBasicFunction.cpp
@@ -3,7 +3,7 @@
 #include<string>
 using namespace std;
 
-void CPPBasicFunction(){
+void run_CPPBasicFunction(){
  string str1="ab";
  string str2="abc";
  cout<<str1.compare(str2)<<endl;
